39fa3156744ea066e936e611f96078eff82e2381
Federico Camiña <federico@icomsalud.com.ar>
2026-09-05 19:42:21 -0300

    Add Supabase configuration to config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// --- CONFIGURACIÓN SUPABASE ---
+const SUPABASE_URL = "https://nwbgghnnviawepfnqktv.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53YmdnaG5udmlhd2VwZm5xa3R2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0MTI4NzMsImV4cCI6MjEwMDk4ODg3M30.tk5dbetd7qxZ0xh1vZLQe71nKdGFeAO65iBZ7xUirCE";
